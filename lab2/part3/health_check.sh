740fcc20c804cf0d25920c3744efabe16e47c1e6
Matthew Acosta <macos052@ucr.edu>
2026-04-15 03:37:54 -0700

    lab 3

--- lab2/part3/health_check.sh
+++ lab2/part3/health_check.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-
+LOGFILE="health.log"
 # Your codes here
+
+# Date and time
+echo "Date: $(date)" >> $LOGFILE
+
+# Hostname
+echo "Hostname: $(hostname)" >> $LOGFILE
+
+# Uptime and load average
+echo "Uptime and Load:" >> $LOGFILE
+uptime >> $LOGFILE
+
+# Disk usage of root (/)
+echo "Disk Usage (/):" >> $LOGFILE
+df -h / >> $LOGFILE
+
+# Memory usage
+echo "Memory Usage:" >> $LOGFILE
+vm_stat >> $LOGFILE
